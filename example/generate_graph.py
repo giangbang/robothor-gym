3b8ce398e77bdd0fe54c2ca31bb99940bad8f5ef
giangbang <banggiangle2015@gmail.com>
2023-07-18 00:07:35 +0700

    add parser in example

--- example/generate_graph.py
+++ example/generate_graph.py
@@ -1,5 +1,6 @@
 import robothor_env
 import gym
+import argparse
 
 
 list_target_object = [
@@ -9,8 +10,21 @@ list_target_object = [
     "Mug"
 ]
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--target-obj", type=str, default=None,
+        help="target object")
+
+    args = parser.parse_args()
+    return args
+
 if __name__ == "__main__":
     env = gym.make("robothor-precompute")
+    args = parse_args()
+
+    if args.target_obj is not None:
+        list_target_object = [args.target_obj]
+
     for obj in list_target_object:
         env.build_graph(target_object=obj)
         graph_file = f"robothor-{env.scene}-{env.target_obj}.pkl"
